51f8ad3a96d674f095c973027abfce559b8a2c77
naseem <naseemkhan702167@gmail.com>
2022-02-28 14:47:20 +0530

    update some package.json file

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,6 +5,9 @@ import Map, { Marker, MapRef } from "react-map-gl";
 // import './tem.css';
 import { useDataContext } from './DataContext';
 
+// mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+// eslint-disable-next-line import/no-webpack-loader-syntax
+
 export default function MapView() {
      const { cityName, geoLocation, colorThem } = useDataContext();
      const { lat, lon } = geoLocation;
